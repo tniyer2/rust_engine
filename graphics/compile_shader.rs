e81976be21703b197229923220b8e4d72e37f6aa
tniyer2 <tanishqiyer88@gmail.com>
2021-04-07 15:37:32 -0400

    5

--- graphics/compile_shader.rs
+++ graphics/compile_shader.rs
@@ -2,11 +2,17 @@
 use shaderc::ShaderKind;
 
 /// Compiles GLSL Source Code into a SPIR-V Binary.
-pub fn compile_shader(glsl: &str, shader_kind: ShaderKind) -> Vec<u32> {
+pub fn compile_shader(source_text: &str, shader_kind: ShaderKind) -> Vec<u32> {
     let mut compiler = shaderc::Compiler::new().unwrap();
 
+    let input_file = "unnamed";
+    let entry_point = "main";
+    let options = None;
+
     compiler
-        .compile_into_spirv(glsl, shader_kind, "unnamed", "main", None)
+        .compile_into_spirv(
+        	source_text, shader_kind,
+        	input_file, entry_point, options)
         .expect("Failed to compile shader")
         .as_binary()
         .to_vec()
