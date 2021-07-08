435ef89068429155150e2775a5905cdc03eca2a7
tniyer2 <tanishqiyer88@gmail.com>
2021-07-07 23:57:52 -0400

    Created State struct in main.rs to store wgpu types used in rendering. State::new sets up access to the wgpu api. State::resize resets the size and recreates the swapchain. State::input, State::update, and State::render, are not implemented yet.
    Created a State in main().
    Event loop in main.rs now checks with State::input if event has already been processed.
    Event loop in main.rs now calls State::resize when window gets resized.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,83 @@
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder
+    window::{Window, WindowBuilder}
 };
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl State {
+    async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+
+        // The instance is a handle to our GPU
+        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+
+        let surface = unsafe { instance.create_surface(window) };
+
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+            },
+        ).await.unwrap();
+
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::default(),
+                label: None,
+            },
+            None, // Trace path
+        ).await.unwrap();
+
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+            format: adapter.get_swap_chain_preferred_format(&surface).unwrap(),
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        Self {
+            surface,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            size
+        }
+    }
+
+    // Set new size and recreate swapchain
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.size = new_size;
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    }
+
+    // Return whether event has been processed
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
+        todo!()
+    }
+}
 
 fn main() {
     env_logger::init();
@@ -14,26 +89,42 @@ fn main() {
         .build(&event_loop)
         .unwrap();
 
+    let mut state = {
+        use futures::executor::block_on;
+        block_on(State::new(&window))
+    };
+
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
             ref event,
             window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested =>
-                *control_flow = ControlFlow::Exit,
-
-            // Exit when escape is pressed.
-            WindowEvent::KeyboardInput {input, ..} => match input {
-                KeyboardInput {
-                    state: ElementState::Pressed,
-                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                    ..
-                } =>
+        } if window_id == window.id() => if !state.input(event) {
+            match event {
+                WindowEvent::CloseRequested =>
                     *control_flow = ControlFlow::Exit,
+
+                // Exit when escape is pressed
+                WindowEvent::KeyboardInput {input, ..} => match input {
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    } =>
+                        *control_flow = ControlFlow::Exit,
+                    _ => {}
+                },
+
+                WindowEvent::Resized(physical_size) => {
+                    state.resize(*physical_size);
+                },
+
+                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    // new_inner_size is &&mut
+                    state.resize(**new_inner_size);
+                }
                 _ => {}
-            },
-            _ => {}
-        },
+            }
+        }
         _ => {}
     });
 }
